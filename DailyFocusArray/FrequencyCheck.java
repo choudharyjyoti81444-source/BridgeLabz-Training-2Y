602b3bc31b9f69cbf6c9fd8061b4ebce691951fd
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-08-11 00:21:37 +0530

    Added Frequency Check practice problem

---
+++ DailyFocusArray/FrequencyCheck.java
@@ -0,0 +1,36 @@
+package com.gla.DSA.PatternPrinting;
+
+import java.util.Scanner;
+
+public class FrequencyCheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an Element:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Frequency of each element:");
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " → " + count);
+        }
+    }
+}
