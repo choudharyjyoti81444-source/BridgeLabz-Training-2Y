e1cd0962378868294c6029ef6d5678c84824b366
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-08-26 20:53:49 +0530

    Added: [Jyoti] My Leetcode Practice

---
+++ LeetcodeProblems/SubArraysOfSizeK.java
@@ -0,0 +1,30 @@
+package com.gla.LeetcodeProblems;
+
+public class SubArraysOfSizeK {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 2, 2, 5, 5, 5, 8};
+        int k = 3;
+        int threshold = 4;
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        if (sum >= k * threshold) {
+            count++;
+        }
+
+        for (int i = k; i < arr.length; i++) {
+            sum = sum - arr[i - k] + arr[i];
+
+            if (sum >= k * threshold) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
