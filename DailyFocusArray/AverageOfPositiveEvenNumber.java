d73b6a679b5704f239e5c7042b9eff75805fafa0
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-08-28 21:15:57 +0530

    Added: [Jyoti] My Daily Focus Array

---
+++ DailyFocusArray/AverageOfPositiveEvenNumber.java
@@ -0,0 +1,29 @@
+package com.gla.DSA.DailyFocusArray;
+
+import java.util.Scanner;
+
+public class AverageOfPositiveEvenNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an Array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " +n+ " Element");
+        for(int i=0; i< arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        int count = 0;
+        for(int i=0; i< arr.length; i++){
+            if(arr[i] > 0 && arr[i] % 2 == 0){
+                sum += arr[i];
+                count++;
+            }
+        }
+        double average = (double) sum/count;
+        System.out.print("Positive Average: " +average);
+    }
+}
