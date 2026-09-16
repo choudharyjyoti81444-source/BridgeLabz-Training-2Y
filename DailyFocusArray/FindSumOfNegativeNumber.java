985bf0dabf1fdabf49f62621e50b9cf5bf624f91
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-09-16 22:21:47 +0530

    Added:[Jyoti] Daily Focus Array

---
+++ DailyFocusArray/FindSumOfNegativeNumber.java
@@ -0,0 +1,29 @@
+package com.gla.DSA.DailyFocusArray;
+
+import java.util.Scanner;
+
+public class FindSumOfNegativeNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an Element:" );
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " +n+ " Element");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < 0){
+                count++;
+                sum += arr[i];
+            }
+        }
+        System.out.print("Sum: " +sum);
+    }
+}
