d837a818f204bb4a63a4b0558c03eb53c97a300e
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-09-18 22:24:32 +0530

    Added:[Jyoti] Practice Problem

---
+++ DailyFocusArray/CountNumberDivisibleBy4.java
@@ -0,0 +1,26 @@
+package com.gla.DSA.DailyFocusArray;
+
+import java.util.Scanner;
+
+public class CountNumberDivisibleBy4 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter an ELement: ");
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+
+    System.out.println("Enter " + n + " Element");
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = sc.nextInt();
+    }
+
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] % 4 == 0) {
+            count++;
+        }
+    }
+    System.out.print("Total Count " + count);
+}
+}
